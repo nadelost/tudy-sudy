8336b257fa0ab8ded3b9153ca6a850c959acdaef
Vlad Chepel <vlad.chepel24@gmail.com>
2022-05-29 21:32:37 +0300

    modified

--- main.py
+++ main.py
@@ -9,4 +9,9 @@ def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
 
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
 bot.infinity_polling()
